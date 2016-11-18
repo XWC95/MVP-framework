db48edf8ef4f5422faa0ef47b09d3a9ec6d125f8
xuwc <xuwc@genvision.com>
2016-11-18 17:44:24 +0800

    测试一下commit

--- app/src/main/java/org/xwc/frameworkdemo/MainActivity.java
+++ app/src/main/java/org/xwc/frameworkdemo/MainActivity.java
@@ -2,6 +2,7 @@ package org.xwc.frameworkdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Log.d("xwc","");
     }
 }
